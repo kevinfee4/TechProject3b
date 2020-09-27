1e10a2e6fefc0d0ac9b64504613a85224c966afb
Kevin <Kevin@DESKTOP-976O6J1>
2020-09-27 15:12:35 -0400

    TechProj3b

--- TechProject3b/Program.cs
+++ TechProject3b/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Kevin Fee
+// 09/24/2020
+using System;
 
 namespace TechProject3b
 {
@@ -6,7 +8,13 @@ namespace TechProject3b
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			// A manually populated array
+			int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+			// Accessing array elements using for loop
+			for (int i = 0; i < numbers.Length; i++)
+				Console.WriteLine("Element Value " + numbers[i]);
+			Console.WriteLine("");
+			Console.ReadKey(true);
 		}
 	}
 }
